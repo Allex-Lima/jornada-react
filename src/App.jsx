63302d958e58f6ada1e045edc2356dcaf80496a6
Allex-Lima <alexlmangabeira@gmail.com>
2024-08-02 17:11:00 -0300

    Feat: interação com JSX

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,37 @@ import styles from './App.module.css'
 import { Post } from './components/Post'
 import { SideBar } from './components/Sidebar'
 
+const posts = [
+  {
+    id: 1,
+    author: {
+      vatarUrl: 'https://avatars.githubusercontent.com/u/73409541?v=4',
+      name: 'Alex Lima',
+      role: 'Desenvolvedor Frontend'
+    },
+    content: [
+      {type: 'paragraph', content: 'Fala galera 🖐'},
+      {type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portfólio. É um projeto que fiz no turn evento da Rocketseat. O nome do projeto é DotorCare.'},
+      {type: 'link', content: 'jane.deseng/doctorcore'},
+    ],
+    publishedAt: new Date('2024-08-02 16:00:00'),
+  },
+  {
+    id: 1,
+    author: {
+      vatarUrl: 'https://avatars.githubusercontent.com/u/73409541?v=4',
+      name: 'Júlia Lima',
+      role: 'Desenvolvedora Frontend'
+    },
+    content: [
+      {type: 'paragraph', content: 'Fala galera 🖐'},
+      {type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portfólio. É um projeto que fiz no turn evento da Rocketseat. O nome do projeto é DotorCare.'},
+      {type: 'link', content: 'jane.deseng/doctorcore'},
+    ],
+    publishedAt: new Date('2024-08-02 16:00:00'),
+  }
+]
+
 function App() {
 
   return (
@@ -14,8 +45,15 @@ function App() {
       <div className={styles.wrapper}>
         <SideBar />
         <main>
-          <Post />
-          <Post />
+         {posts.map((post) => {
+          return (
+            <Post 
+              author={post.author}
+              content={post.content}
+              publishedAt={post.publishedAt}
+            />
+          )
+         })}
         </main>
       </div>
 
