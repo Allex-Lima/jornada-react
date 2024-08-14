47a2495af1776a74e33a5f78c94c996f77de18ae
Allex-Lima <alexlmangabeira@gmail.com>
2024-08-14 18:29:40 -0300

    Feat: comunicação entre componentes

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -33,6 +33,11 @@ export function Post({ author, publishedAt, content }) {
         setNewCommentText(event.target.value)
     }
 
+    function deleteComment(comment) {
+        console.log(`Deletar comentário ${comment}`)
+        
+    }
+
     return (
         <article className={styles.post}>
             <header>
@@ -73,7 +78,12 @@ export function Post({ author, publishedAt, content }) {
 
             <div className={styles.commentList}>
                 {comments.map((comment) => {
-                    return <Comment key={comment} content={comment} />
+                    return (
+                    <Comment
+                        key={comment} 
+                        content={comment} 
+                        onDeleteComment={deleteComment}
+                    />)
                 })}
             </div>
         </article>
